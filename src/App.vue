8282b0739382e927f9c9eada2cca8e05284417f3
Alea <1094941478@qq.com>
2021-03-26 11:36:17 +0800

    to

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <template>
   <div id="app">
     <div id="nav">
-
     </div>
     <router-view />
   </div>
 </template>
+<script>
+import { removeWatermark, setWaterMark } from '@/common/js/watermark'; 
+
+export default {
+  data(){
+    return{
+    }
+  },
+  mounted() {
+  setWaterMark('111222', '1112441');
+  },
+  destroyed() {
+    removeWatermark();
+}
+
+}
+</script>
 
 <style lang="scss">
 * {
